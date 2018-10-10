63f3d5e26940ae9995adcd16d7a06c8bc130887d
unknown <mwann031@uottawa.ca>
2018-10-10 16:17:21 -0400

    clean up

--- rent_calculator.py
+++ rent_calculator.py
@@ -14,13 +14,13 @@ manisha_owe = float("{0:.2f}".format(0.3*rent_amount + hydro_internet_portion))
 print("Note: Manisha pays the rent each month.")
 print("Note: Jane pays the internet and hydro each month, these are deducted from the amount she owes Manisha.\n")
 
-print("Summary:")
+print("Confirmation:")
 print("Jane amount: " + str(float("{0:.2f}".format(internet_bill + rent_amount*0.4 - (internet_bill/3)*2))))
 print("Joy amount: " + str(float("{0:.2f}".format(rent_amount*0.3 + (internet_bill/3)))))
 print("Manisha amount: " + str(float("{0:.2f}".format(rent_amount - rent_amount*0.3 - rent_amount*0.4 + internet_bill/3)))+"\n")
 
 
-
+print("Transfers:")
 print("Jane needs to transfer $"+ str(jane_owe) + " to Manisha")
 print("Joy needs to transfer $"+ str(joy_owe) + " to Manisha \n")
 
