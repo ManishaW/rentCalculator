ec4175b2dbe01b33be155efc6afffca81e6b1813
unknown <mwann031@uottawa.ca>
2018-10-10 16:46:02 -0400

    calculation corrections

--- rent_calculator.py
+++ rent_calculator.py
@@ -5,19 +5,20 @@ internet_bill = 53 #constant
 
 hydro = input("What is the hydro bill amount? ")
 hydro_internet_portion = (int(hydro) + internet_bill)
+print(hydro_internet_portion)
 
 joy_owe = float("{0:.2f}".format(0.3*rent_amount + (hydro_internet_portion/3)))
 jane_owe = float("{0:.2f}".format(0.4*rent_amount - (hydro_internet_portion/3)*2))
-manisha_owe = float("{0:.2f}".format(0.3*rent_amount + hydro_internet_portion))
+manisha_owe = float("{0:.2f}".format(0.3*rent_amount + hydro_internet_portion/3))
 
 
 print("Note: Manisha pays the rent each month.")
 print("Note: Jane pays the internet and hydro each month, these are deducted from the amount she owes Manisha.\n")
 
 print("Confirmation:")
-print("Jane amount: " + str(float("{0:.2f}".format(internet_bill + rent_amount*0.4 - (internet_bill/3)*2))))
-print("Joy amount: " + str(float("{0:.2f}".format(rent_amount*0.3 + (internet_bill/3)))))
-print("Manisha amount: " + str(float("{0:.2f}".format(rent_amount - rent_amount*0.3 - rent_amount*0.4 + internet_bill/3)))+"\n")
+print("Jane total: " + str(float("{0:.2f}".format(hydro_internet_portion + rent_amount*0.4 - (hydro_internet_portion/3)*2))))
+print("Joy total: " + str(float("{0:.2f}".format(rent_amount*0.3 + (hydro_internet_portion/3)))))
+print("Manisha total: " + str(float("{0:.2f}".format(rent_amount - rent_amount*0.3 - rent_amount*0.4 + hydro_internet_portion/3)))+"\n")
 
 
 print("Transfers:")
