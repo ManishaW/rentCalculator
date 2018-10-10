33573aea60c01dc8f0cb57c8be5c0f8e2bad19fe
unknown <mwann031@uottawa.ca>
2018-10-10 16:05:01 -0400

    basic functionality

---
+++ rent_calculator.py
@@ -0,0 +1,27 @@
+print("Welcome to the rent calculator! lol")
+rent_amount = 1046.15 #constant
+internet_bill = 53 #constant
+
+
+hydro = input("What is the hydro bill amount? ")
+hydro_internet_portion = (int(hydro) + internet_bill)
+
+joy_owe = float("{0:.2f}".format(0.3*rent_amount + (hydro_internet_portion/3)))
+jane_owe = float("{0:.2f}".format(0.4*rent_amount - (hydro_internet_portion/3)*2))
+manisha_owe = float("{0:.2f}".format(0.3*rent_amount + hydro_internet_portion))
+
+
+print("Note: Manisha pays the rent each month.")
+print("Note: Jane pays the internet and hydro each month, these are deducted from the amount she owes Manisha.\n")
+
+print("Summary:")
+print("Jane amount: " + str(float("{0:.2f}".format(internet_bill + rent_amount*0.4 - (internet_bill/3)*2))))
+print("Joy amount: " + str(float("{0:.2f}".format(rent_amount*0.3 + (internet_bill/3)))))
+print("Manisha amount: " + str(float("{0:.2f}".format(rent_amount - rent_amount*0.3 - rent_amount*0.4 + internet_bill/3)))+"\n")
+
+
+
+print("Jane needs to transfer $"+ str(jane_owe) + " to Manisha")
+print("Joy needs to transfer $"+ str(joy_owe) + " to Manisha \n")
+
+
